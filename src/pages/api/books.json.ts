0d4295f5e91d9a347c5e98e7de37d7d5a6ce11a6
Chris Pennington <chris@codinginpublic.dev>
2024-11-18 12:46:30 -0700

    lesson-149

---
+++ src/pages/api/books.json.ts
@@ -0,0 +1,45 @@
+import type { OpenLibraryDocsItem } from "@/types/OpenLibrary";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  // get query params
+
+  // return early if no params
+
+  // query the API endpoint
+  try {
+    const res = await fetch(
+      "https://openlibrary.org/search.json?q=the+lord+of+the+rings&limit=6"
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch books");
+    }
+
+    // get back books data
+    const data = await res.json();
+    // query the response for the data we want
+    const books = data.docs.map((book: OpenLibraryDocsItem) => ({
+      title: book.title,
+      author: book.author_name,
+      cover: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
+      id: book.key.replaceAll("/works/", ""),
+    }));
+
+    // return
+    return new Response(
+      JSON.stringify({
+        data: books,
+        error: null,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: error instanceof Error ? error.message : error,
+      })
+    );
+  }
+};
