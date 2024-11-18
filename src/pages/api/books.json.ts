d4ed33eceecedb6dedc85e722cf68905eba9b7c5
Chris Pennington <chris@codinginpublic.dev>
2024-11-18 13:30:20 -0700

    lesson-152

--- src/pages/api/books.json.ts
+++ src/pages/api/books.json.ts
@@ -1,5 +1,6 @@
 import type { OpenLibraryDocsItem } from "@/types/OpenLibrary";
 import type { APIRoute } from "astro";
+import { db, Book } from "astro:db";
 
 export const GET: APIRoute = async ({ request }) => {
   // get query params
@@ -44,6 +45,34 @@ export const GET: APIRoute = async ({ request }) => {
     );
   } catch (error) {
     console.error(error);
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: error instanceof Error ? error.message : error,
+      })
+    );
+  }
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+
+  try {
+    if (!data || !data.title || !data.author || !data.cover || !data.id) {
+      // check each type and sanitize
+      throw new Error("No book selected");
+    }
+
+    const dbData = await db.insert(Book).values({ ...data, status: "to_read" });
+
+    return new Response(
+      JSON.stringify({
+        data: dbData,
+        error: null,
+      })
+    );
+  } catch (error) {
+    console.log(error);
     return new Response(
       JSON.stringify({
         data: null,
