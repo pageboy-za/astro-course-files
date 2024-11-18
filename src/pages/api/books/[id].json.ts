19c80459f90c2283e1ae592e149bfe2c40ee236a
Chris Pennington <chris@codinginpublic.dev>
2024-11-18 16:38:50 -0700

    lesson-156

---
+++ src/pages/api/books/[id].json.ts
@@ -0,0 +1,35 @@
+import type { APIRoute } from "astro";
+import { Book, db, eq } from "astro:db";
+
+export const PATCH: APIRoute = async ({ request, params }) => {
+  const { id } = params;
+  const data = await request.json();
+
+  try {
+    if (!data || !data.status || !id) {
+      throw new Error("No book selected");
+    }
+
+    const dbData = await db
+      .update(Book)
+      .set({
+        status: data.status,
+      })
+      .where(eq(Book.id, id));
+
+    return new Response(
+      JSON.stringify({
+        data: dbData,
+        error: null,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: error instanceof Error ? error.message : error,
+      })
+    );
+  }
+};
